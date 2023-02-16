526ac9e807812ae2d6149a4b577a09a5601fe89d
Max Struijs <maxstruys32@gmail.com>
2023-02-16 12:53:05 +0100

    Alarm done

--- Alarm.go
+++ Alarm.go
@@ -1,1 +1,24 @@
 package main
+
+import "fmt"
+
+// Give input
+func main() {
+	var input int
+	_input := GetUserInput()
+	input = _input
+	DoTheLoop(input)
+}
+
+func GetUserInput() int {
+	var input int
+	fmt.Println("How many times should the alarm go off?")
+	fmt.Scan(&input)
+	return input
+}
+
+func DoTheLoop(_input int) {
+	for i := 1; i <= _input; i++ {
+		fmt.Println("Alarm ", i, "!")
+	}
+}
